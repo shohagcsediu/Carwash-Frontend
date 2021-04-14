dceba8e1d1eb00e673470b3ede73d12ee62e3c8c
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-14 23:44:06 +0600

    added homepage doctor part

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
+import Doctors from '../Doctors/Doctors';
 import Header from '../Header/Header';
 import Services from '../Services/Services';
 
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Header></Header>
             <Services></Services>
+            <Doctors></Doctors>
             <Footer></Footer>
         </div>
     );
